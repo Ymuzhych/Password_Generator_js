f23f5988988c3b4c6402d7cc6490f0f13daa3f90
Yuliia M <myuliia2018@gmail.com>
2022-04-10 15:54:40 -0500

    combined code to promt questiond, adding arrays, and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,17 @@
 // Assignment code here
 
+//Combined code to prompt questions 
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// Add arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add variables
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
